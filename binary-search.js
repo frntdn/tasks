f8b60b33737eb83f299fc22d588394ea33e59875
Denis Osipov <d.osipov@rambler-co.ru>
2018-03-26 14:56:47 +0300

    improve binary-search

--- binary-search.js
+++ binary-search.js
@@ -1,16 +1,32 @@
 'use strict'
 
+let isFirstCheck = true
+
 function binarySearch(array, item) {
   const arrLength = array.length
+
+  if (isFirstCheck && array[arrLength - 1] < item)
+    return 'no item in array'
+  else
+    isFirstCheck = false
+
   if (!arrLength)
     return 'no item in array'
-  if (arrLength === 1 && array[0] === item)
-    return `found it = ${item}`
+
+  if (arrLength === 1)
+    if (array[0] === item)
+      return `found it = ${item}`
+    else
+      return 'no item in array'
+
   const roundedIndex = Math.round(arrLength / 2)
   const center = array[roundedIndex]
+
   if (center === item)
     return `binary search in action = ${item}`
+
   const _array = (center < item) ? array.slice(roundedIndex) : array.slice(0, roundedIndex)
+
   return binarySearch(_array, item)
 }
 
@@ -19,6 +35,10 @@ for (let i = 0; i < 10000000; i++)
   array.push(i)
 
 
-const start = Date.now()
+// const start = Date.now()
 console.log(binarySearch(array, 9131))
-console.log('finish:', Date.now() - start)
+// console.log('finish:', Date.now() - start)
+
+const list = [1, 2, 4, 5, 6, 7, 8, 10]
+console.log(binarySearch(list, 3))
+console.log(binarySearch(list, 15))
